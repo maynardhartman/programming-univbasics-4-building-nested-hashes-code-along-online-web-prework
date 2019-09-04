bc3b842d4daf1841c83dc3bdff21621ddc2cf3c5
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-04 05:15:38 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -17,7 +17,7 @@ def bonus
       :heroine_friends => [
           {name: "Mercutio", age: "18", attitude: "hot-headed"}, 
           {name: "Nurse", age: "44", attitude: "worried"}
-      ]
+      ]}
    
   }
 }
